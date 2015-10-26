acefb5ab79bf4f193f538f559090185016c6c7e8
任beelives <rbeelives@gmail.com>
2015-10-26 23:04:05 +0800

    添加啊函数Tk界面初始化

--- _src/om2py2w/2wex0/main.py
+++ _src/om2py2w/2wex0/main.py
@@ -10,42 +10,46 @@ from Tkinter import *
 5 界面构建
 """
 
-root = Tk()
-root.title('笔记')
+def run():
+    root = Tk()
+    root.title('笔记')
 
 # 创建容器
-main=Frame()
-r=Frame()
-but=Frame()
+    main=Frame()
+    r=Frame()
+    but=Frame()
 
 # 创建元素
-logo=Label(r,text='Readm')
-text=Text(main)
-b=Button(but,text='走着')
+    logo=Label(r,text='Readm')
+    text=Text(main)
+    b=Button(but,text='走着')
 
 
 
 # 位置
-main.grid(row=0, column=0, padx=2, pady=5)
-but.grid(row=1, column=0, padx=2, pady=5)
-r.grid(row=0, column=1, padx=1, pady=1)
-"""
+    main.grid(row=0, column=0, padx=2, pady=5)
+    but.grid(row=1, column=0, padx=2, pady=5)
+    r.grid(row=0, column=1, padx=1, pady=1)
+    """
 
 grid() 参数说明
 row:行
 columu:列
 padx, pady: 多少部件的像素，水平和垂直方向
 grid_propagate 父组将集合大小有组件控制
-"""
+    """
 
-main.grid_propagate(0)
-but.grid_propagate(0)
-r.grid_propagate(0)
+    main.grid_propagate(0)
+    but.grid_propagate(0)
+    r.grid_propagate(0)
 
 
 
-logo.pack()
-text.pack()
-b.pack()
+    logo.pack()
+    text.pack()
+    b.pack()
 
-root.mainloop()
+    root.mainloop()
+if __name__=="__main__":
+    run()
+
